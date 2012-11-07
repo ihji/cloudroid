a51faa1babe870f1e2afacf6a05ea43906fdb0b6
Heejong Lee <heejong@gmail.com>
2012-11-07 11:34:19 +0900

    auto refresh webpage

--- server.py
+++ server.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('status.html',
-           status_map = daemon.client_status,
-           analysis_queue = daemon.analysis_queue)
+    return render_template('status.html')
+
+@app.route('/workerlist')
+def workerlist():
+    wlist = ""
+    for status in daemon.client_status.items():
+        wlist += "<tr><td>{}</td><td>{}</td></tr>\n".format(status[0],status[1])
+    return wlist
+
+@app.route('/queuesize')
+def queuesize():
+    return str(daemon.analysis_queue.qsize())
 
 if __name__ == '__main__':
     daemon.launch()
